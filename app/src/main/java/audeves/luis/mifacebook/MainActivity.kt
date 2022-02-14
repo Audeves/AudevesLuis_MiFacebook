8cc69b82fa81889e2d5ca4d2f98477ac7d10722c
audeves <audeves01@gmail.com>
2022-02-14 14:07:37 -0700

    app facebook

---
+++ app/src/main/java/audeves/luis/mifacebook/MainActivity.kt
@@ -0,0 +1,11 @@
+package audeves.luis.mifacebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.registro_face)
+    }
+}
---
+++ app/src/main/java/audeves/luis/mifacebook/MainActivity2.kt
@@ -0,0 +1,11 @@
+package audeves.luis.mifacebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.inicio_face)
+    }
+}
